71e3b8878e3e3cb4710016b663bae221825c857d
Karthik1511 <sreekarthik1511@gmail.com>
2024-11-14 15:23:31 +0530

    intitialisation

---
+++ src/main/java/com/app/demo/java_maven_ex/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.java_maven_ex;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Karthik 22BD1A050V");
+    }
+}
